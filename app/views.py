6f6c9a13ccafbb9b6be3ac693e13ffcfbe6f9008
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-09-20 18:24:28 +0200

    More rows, still no loop

--- app/views.py
+++ app/views.py
@@ -15,12 +15,12 @@ def show_time():
         now=time.strftime("%c")
     )
 
+
 @application.route('/stock')
 def stock():
+    stocks = [{"name": "Spasmalgon", "qt": "1", "form": "Tablet", "action": "Spasmolytic drugs"},
+              {"name": "Spasmalgon2", "qt": "2", "form": "Tablet2", "action": "Spasmolytic 2drugs"}]
     return render_template(
         'stock.html',
-        name="Spasmalgon",
-        qt="1",
-        form="Tablet",
-        action="Spasmolytic drugs"
+        stocks=stocks
     )
